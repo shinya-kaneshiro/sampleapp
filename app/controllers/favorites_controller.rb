136d5def8ad3a4add4e14a9d98f934c664ae777d
kaneshiro-shinya <armageddn1@gmail.com>
2021-05-25 22:05:18 +0900

    point_005_Before FontAwsome

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,15 +1,22 @@
 class FavoritesController < ApplicationController
 
   def create
-    favorite = current_user.favorites.build(post_id: params[:post_id])
-    favorite.save
-    redirect_to posts_path
+    @favorite = current_user.favorites.build(post_id: params[:post_id])
+    @favorite.save
+    @post = @favorite.post
+    
+    # favorite = current_user.favorites.build(post_id: params[:post_id])
+    # favorite.save
+
+    # redirect_to posts_path
   end
 
   def destroy
-    favorite = Favorite.find_by(post_id: params[:post_id], user_id: current_user.id)
-    favorite.destroy
-    redirect_to posts_path
+    @favorite = Favorite.find_by(post_id: params[:post_id], user_id: current_user.id)
+    @favorite.destroy
+    @post = @favorite.post
+
+    # redirect_to posts_path
   end
   
 end
